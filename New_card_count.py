204f223e35eb1728c5214041a9c9b3639b38e51f
JuanTheCoder2012 <107782146+JuanTheCoder2012@users.noreply.github.com>
2023-10-03 14:28:07 -0400

    it finally works on pc managed to Declare running_count out of the while loop.

--- New_card_count.py
+++ New_card_count.py
@@ -5,7 +5,7 @@ def calculate_running_count(card_value, count):
         return count - 1
     else:
         return count
-    
+
 def calculate_true_count(running_count, remaining_decks):
     return running_count / remaining_decks
 
@@ -16,22 +16,22 @@ def advise_bet(true_count):
         return "Bet normal."
     else:
         return "Bet less."
-    
+
 def main():
     decks = 8
     cards_per_deck = 52
     total_cards = decks * cards_per_deck
-    runing_count = 0
-    
+    running_count = 0  # Declare running_count outside the while loop
+
     print("Welcome to the 8-deck card counting game!")
     print("Enter card values (2-10, J, Q, K, A). Type 'exit' to quit.")
-    
+
     while True:
-        card_input = input("Enter a card value:  ").strip().lower()
-        
+        card_input = input("Enter a card value: ").strip().lower()
+
         if card_input == 'exit':
             break
-        
+
         if card_input in ['2', '3', '4', '5', '6']:
             card_value = int(card_input)
         elif card_input in ['10', 'j', 'q', 'k', 'a']:
@@ -39,15 +39,14 @@ def main():
         else:
             print("Invalid input. Please enter a valid card value.")
             continue
-        
+
         running_count = calculate_running_count(card_value, running_count)
         remaining_decks = (total_cards - len(card_input.split())) / cards_per_deck
         true_count = calculate_true_count(running_count, remaining_decks)
-        
-        print(f"Running Count {running_count}")
+
+        print(f"Running Count: {running_count}")
         print(f"True Count: {true_count:.2f}")
         print(advise_bet(true_count))
-        
+
 if __name__ == "__main__":
     main()
-    
