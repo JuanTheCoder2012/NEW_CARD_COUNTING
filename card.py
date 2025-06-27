29f15afe7b1c197a10366d274dc56067eb5c4f84
Juan C. Cabrera-Flores <107782146+JuanTheCoder2012@users.noreply.github.com>
2025-06-26 21:58:30 -0400

    Improve card counting app with GUI and reset

--- card.py
+++ card.py
@@ -1,4 +1,4 @@
-NUM_ON_CARDS= {
+NUM_ON_CARDS = {
     '2': 1,
     '3': 1,
     '4': 1,
@@ -13,10 +13,13 @@ NUM_ON_CARDS= {
     'K': -1,
     'A': -1,
 }
-deck = 8
+
+DECKS = 8
+
+
 def main():
     while True:
-        inp = input('Enter a card or "exit" to quit')
+        inp = input('Enter a card or "exit" to quit: ')
         if inp.lower() == 'exit':
             break
         elif not inp:
@@ -25,11 +28,11 @@ def main():
         count = 0
         for card in inp:
             count += NUM_ON_CARDS.get(card.upper(), 0)
-            played = cards / 52.0
-            truecount = count / (deck - played)
+        played = cards / 52.0
+        truecount = count / (DECKS - played)
         print(f'Count: {count}')
         print(f'True count: {truecount}')
+
+
 if __name__ == '__main__':
     main()
-    
-    
